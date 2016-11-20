4d0bdba4c241ea8b66535cf9adad36d2742a1ab9
amotttier <alex_mottier@hotmail.com>
2016-11-20 15:59:22 +0100

    sql

--- database/tip.sql
+++ database/tip.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.6.0
+-- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Nov 19, 2016 at 04:53 PM
+-- Generation Time: Nov 20, 2016 at 06:58 AM
 -- Server version: 10.1.9-MariaDB-log
--- PHP Version: 7.0.10
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,11 +32,20 @@ CREATE TABLE `t_user` (
   `first_name` varchar(25) NOT NULL,
   `last_name` varchar(60) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `fk_team` int(10) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_user`
+--
+
+INSERT INTO `t_user` (`id`, `username`, `first_name`, `last_name`, `password`, `salt`, `email`, `fk_team`, `created_at`, `updated_at`) VALUES
+(1, 'amottier', 'Alexandre', 'Mottier', 'ce4a8755b9da893d7a7948170bfe662f1d24f0bc', 'ÞPÐP|ì\\rï\\x02ƒD¤"\\x01\\x01', 'alex_mottier@hotmail.com', 1, '2016-11-20 13:49:57', '2016-11-20 13:58:29'),
+(2, 'emujkanovic', 'Edin', 'Mujkanovic', 'bfc02f7952cce08e2b2370197d449844efd9660c', '»á\\x05Ñ‡óýtÍ\\x02ÃJÕjN', 'edin.mujkanovic@farmont.ch', 1, '2016-11-20 14:58:38', '2016-11-20 14:58:38');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +67,7 @@ ALTER TABLE `t_user`
 -- AUTO_INCREMENT for table `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
